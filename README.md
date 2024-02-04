5a1619eee9c77ee2d14a354331eb78f44b04e73e
koray uzun <korayuzun22@gmail.com>
2024-02-04 21:04:43 +0300

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # whatsapp_bot
+
+in 5 sec you need to select your friend in whatsapp and it will push messages
---
+++ main.py
@@ -0,0 +1,8 @@
+import pyautogui
+import time
+
+time.sleep(5)
+while True:
+    pyautogui.typewrite("yaptım")
+    pyautogui.press('enter')
+    time.sleep(0.1)
